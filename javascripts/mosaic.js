9b7df817b4c11d1bdaf114a8a62c7e263dfbf9b6
Giovanni Cappellotto <potomak84@gmail.com>
2020-02-22 23:40:38 -0500

    Add overlay
    The overlay can be customized by setting the `overlay` query
    parameter to a 64 bytes string of `0`s and `1`s.

--- javascripts/mosaic.js
+++ javascripts/mosaic.js
@@ -6,6 +6,117 @@
 // * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
 
 (function () {
+  // Configuration
+  var scaleFactor = 20;
+  var samples = Math.ceil(scaleFactor * scaleFactor / 100);
+
+  var tr = [0, 0, 0, 0];
+  var smileyColor = randColor();
+  var smileys = [
+    [
+      '00000000',
+      '00111100',
+      '01011010',
+      '01111110',
+      '01111110',
+      '00100100',
+      '00000000',
+      '00000000',
+    ],
+    [
+      '00000000',
+      '00000000',
+      '00100100',
+      '01111110',
+      '01111110',
+      '00111100',
+      '00011000',
+      '00000000',
+    ],
+    [
+      '00000000',
+      '00000000',
+      '00100100',
+      '00000000',
+      '01000010',
+      '00111100',
+      '00000000',
+      '00000000',
+    ],
+    [
+      '00000000',
+      '00000000',
+      '00100100',
+      '00000000',
+      '00000000',
+      '00111100',
+      '00000000',
+      '00000000',
+    ],
+    [
+      '00000000',
+      '00000000',
+      '00100100',
+      '00000000',
+      '00000000',
+      '00111100',
+      '01000010',
+      '00000000',
+    ],
+    [
+      '00000000',
+      '01000010',
+      '00100100',
+      '00011000',
+      '00011000',
+      '00100100',
+      '01000010',
+      '00000000',
+    ]
+  ];
+  var smiley = (function () {
+    var overlayIndex = document.location.search.indexOf('overlay=');
+    if (overlayIndex > -1) {
+      var overlay = document.location.search.slice(overlayIndex);
+      overlay = overlay.split('&')[0];
+      var overlayData = overlay.split('=')[1];
+      try {
+        return decodeOverlay(overlayData);
+      } catch(err) {
+        console.error('Overlay data decoding failed :( -', err);
+      }
+    }
+    return smileys[randInt(smileys.length)];
+  })();
+
+  function decodeOverlay(data) {
+    if (data.length != 64) {
+      throw new Error('Wrong data length');
+    }
+    var i, overlay = [];
+    for (i = 0; i < 8; i++) {
+      overlay.push(data.slice(i * 8, i * 8 + 8));
+    }
+    return overlay;
+  }
+
+  function randColor() {
+    var comp = 32;
+    var highComponent1 = randInt(3);
+    var highComponent2 = randInt(3);
+    var color = [
+      randInt(comp),
+      randInt(comp),
+      randInt(comp),
+      0.25 + Math.random() / 2
+    ];
+    color[highComponent1] += 255 - comp;
+    if (highComponent1 != highComponent2) {
+      color[highComponent2] += 255 - comp;
+    }
+    return color;
+  };
+
   function getColorAtCoord(x, y, imageData) {
     var redIndex = y * (imageData.width * 4) + x * 4;
     var greenIndex = redIndex + 1;
@@ -38,14 +149,31 @@
     return res;
   }
 
+  function overlay(bitmap, sampledData) {
+    var x, y, sx, sy, overlayData = [];
+    var dw = Math.ceil(sampledData[0].length / bitmap[0].length);
+    var dh = Math.ceil(sampledData.length / bitmap.length);
+
+    for (y = 0; y < sampledData.length; y++) {
+      overlayData.push([]);
+      sy = Math.floor(y / dh);
+      for (x = 0; x < sampledData[y].length; x++) {
+        sx = Math.floor(x / dw);
+        overlayData[y].push(bitmap[sy][sx] == '1' ? smileyColor : tr);
+      }
+    }
+
+    return overlayData;
+  }
+
   function sample(imageData, scaleFactor, samples) {
-    var x, y, i, ri, colorSample, averageColor, sampled = [];
+    var x, y, i, ri, colorSample, averageColor, sampledData = [];
 
     var scaledWidth = Math.floor(imageData.width / scaleFactor);
     var scaledHeight = Math.floor(imageData.height / scaleFactor);
 
     for (y = 0; y < scaledHeight; y++) {
-      sampled[y] = [];
+      sampledData.push([]);
       for (x = 0; x < scaledWidth; x++) {
         averageColor = [0, 0, 0, 0];
         for (i = 0; i < samples; i++) {
@@ -65,16 +193,22 @@
         averageColor[2] = Math.round(averageColor[2] / samples);
         averageColor[3] = Math.round(averageColor[3] / samples);
 
-        sampled[y].push(averageColor);
+        sampledData[y].push(averageColor);
       }
     }
 
-    return sampled;
+    return sampledData;
   }
 
-  // Configuration
-  var scaleFactor = 30;
-  var samples = Math.ceil(scaleFactor * scaleFactor / 100);
+  function draw(ctx, data, scaleFactor) {
+    var x, y;
+    for (y = 0; y < data.length; y++) {
+      for (x = 0; x < data[y].length; x++) {
+        ctx.fillStyle = 'rgba(' + data[y][x][0] + ', ' + data[y][x][1] + ', ' + data[y][x][2] + ', ' + data[y][x][3] + ')'
+        ctx.fillRect(x * scaleFactor, y * scaleFactor, scaleFactor, scaleFactor);
+      }
+    }
+  }
 
   var target = document.getElementById('mosaic');
   var canvas = document.createElement('canvas');
@@ -86,14 +220,10 @@
     canvas.height = Math.floor(img.naturalHeight / scaleFactor) * scaleFactor;
     ctx.drawImage(img, 0, 0);
     var imageData = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
-    var sampled = sample(imageData, scaleFactor, samples);
-    var x, y;
-    for (y = 0; y < sampled.length; y++) {
-      for (x = 0; x < sampled[y].length; x++) {
-        ctx.fillStyle = 'rgba(' + sampled[y][x][0] + ', ' + sampled[y][x][1] + ', ' + sampled[y][x][2] + ', ' + sampled[y][x][3] + ')'
-        ctx.fillRect(x * scaleFactor, y * scaleFactor, scaleFactor, scaleFactor);
-      }
-    }
+    var sampledData = sample(imageData, scaleFactor, samples)
+    draw(ctx, sampledData, scaleFactor);
+    var overlayData = overlay(smiley, sampledData);
+    draw(ctx, overlayData, scaleFactor);
   };
   img.src = target.dataset.src;
 }());
